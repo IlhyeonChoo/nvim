8cb9dd47075e68725d62231daab2883ac612ca6d
ilhyeongpu <ilhyeonchu@gmail.com>
2026-03-24 16:44:55 +0000

    fix docker

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -13,6 +13,10 @@ if has_add then
     },
     filename = {
       Jenkinsfile = "groovy",
+      ["docker-compose.yml"] = "yaml.docker-compose",
+      ["docker-compose.yaml"] = "yaml.docker-compose",
+      ["compose.yml"] = "yaml.docker-compose",
+      ["compose.yaml"] = "yaml.docker-compose",
     },
     pattern = {
       [".*%.env%..+"] = "sh",
--- lua/lsp/servers/docker_compose_language_service.lua
+++ lua/lsp/servers/docker_compose_language_service.lua
@@ -1,6 +1,5 @@
 -- docker-compose LSP 설정
 return function()
-  -- docker compose 파일에만 제한된 언어 서버 활용
   local compose_files = {
     "docker-compose.yml",
     "docker-compose.yaml",
@@ -9,13 +8,7 @@ return function()
   }
 
   return {
-    filetypes = { "yaml" },
-    root_dir = function(fname)
-      local util = require("lspconfig.util")
-      local unpack_fn = table.unpack or unpack
-      local detect = util.root_pattern(unpack_fn(compose_files))
-      local root = detect(fname)
-      return root or util.find_git_ancestor(fname)
-    end,
+    filetypes = { "yaml.docker-compose" },
+    root_markers = compose_files,
   }
 end
